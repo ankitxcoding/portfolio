5e070d1b23f7fd958b84c3cd806e96f0dd0f4b05
ankitxcoding <ankitxcoding@gmail.com>
2024-03-08 18:29:33 +0530

    website completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,7 @@ import Navigation from "./Navigation";
 import FollowLinks from "./FollowLinks";
 import { useEffect, useState } from "react";
 import { GITHUB_API } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 const About = () => {
   const [github, setGithub] = useState([]);
@@ -23,7 +24,14 @@ const About = () => {
     <>
       <ParticlesBackground />
       <div className="flex flex-col items-center justify-center h-screen">
-        <img src={avatar_url} alt="profilePicture" className="w-80 rounded-full border-2 border-white"/>
+        <Link to="/" className="m-5 absolute left-0 top-0">
+          <i className="fa-solid fa-house text-white text-4xl hover:scale-125"></i>
+        </Link>
+        <img
+          src={avatar_url}
+          alt="profilePicture"
+          className="w-80 rounded-full border-2 border-white"
+        />
         <p className="m-1 p-1 text-white font-semibold text-2xl w-3/4">
           Hello, I&apos;m Ankit Vishwakarma, a student of Bachelor of Technology
           in Computer Science and Engineering at Veer Bahadur Singh Purvanchal
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,50 @@
 import ParticlesBackground from "./ParticlesBackground";
 import Navigation from "./Navigation";
 import FollowLinks from "./FollowLinks";
+import { Link } from "react-router-dom";
 
 const Projects = () => {
   return (
     <>
       <ParticlesBackground />
-      <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="m-1 p-1 text-white font-bold text-8xl">Projects</h1>
+      <div className="flex flex-col items-center justify-center">
+        <Link to="/" className="m-5 absolute left-0 top-0">
+          <i className="fa-solid fa-house text-white text-4xl hover:scale-125"></i>
+        </Link>
+        <h1 className="m-1 p-1 text-white font-bold text-6xl">Projects</h1>
+        <a
+          href="https://foodz-nine.vercel.app/"
+          className="m-4 text-white text-3xl font-semibold hover:text-green-500"
+        >
+          <i className="fa-solid fa-link text-xl"></i> Foodz
+        </a>
+        <iframe
+          src="https://foodz-nine.vercel.app/"
+          title="foodz"
+          className="w-4/5 m-4 h-[40rem] rounded-md shadow-md shadow-gray-300 bg-white"
+        ></iframe>
+        <a
+          href="https://ankitxcoding.github.io/eCommerce/"
+          className="m-4 text-white text-3xl font-semibold hover:text-yellow-500"
+        >
+          <i className="fa-solid fa-link text-xl"></i> eCommerce
+        </a>
+        <iframe
+          src="https://ankitxcoding.github.io/eCommerce/"
+          title="eCommerce"
+          className="w-4/5 m-4 h-[40rem] rounded-md shadow-md shadow-gray-300"
+        ></iframe>
+        <a
+          href="https://ankitxcoding.github.io/MoviesAndTVShows/"
+          className="m-4 text-white text-3xl font-semibold hover:text-amber-500"
+        >
+          <i className="fa-solid fa-link text-xl"></i> MoviesAndTVShows
+        </a>
+        <iframe
+          src="https://ankitxcoding.github.io/MoviesAndTVShows/"
+          title="MoviesAndTVShows"
+          className="w-4/5 m-4 h-[40rem] rounded-md shadow-md shadow-gray-300"
+        ></iframe>
         <FollowLinks />
         <Navigation />
       </div>
