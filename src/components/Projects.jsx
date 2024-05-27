93778e585e512ad0390457cdf709958bc100bf06
ankitxcoding <ankitxcoding@gmail.com>
2024-05-27 11:29:38 +0530

    project update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,9 @@ const Projects = () => {
         <Link to="/" className="m-5 absolute left-0 top-0">
           <i className="fa-solid fa-house text-white text-4xl hover:scale-125"></i>
         </Link>
-        <h1 className="m-1 p-1 text-white font-bold text-4xl sm:text-4xl md:text-5xl lg:text-5xl xl:text-6xl">Projects</h1>
+        <h1 className="m-1 p-1 text-white font-bold text-4xl sm:text-4xl md:text-5xl lg:text-5xl xl:text-6xl">
+          Projects
+        </h1>
         <a
           href="https://foodz-nine.vercel.app/"
           className="m-4 text-white text-3xl font-semibold hover:text-green-500"
@@ -24,6 +26,17 @@ const Projects = () => {
           className="w-4/5 m-4 h-[40rem] rounded-md shadow-md shadow-gray-300"
         ></iframe>
         <a
+          href="https://movieznseriez.vercel.app/"
+          className="m-4 text-white text-3xl font-semibold hover:text-amber-500"
+        >
+          <i className="fa-solid fa-link text-xl"></i> MoviezNSeriez
+        </a>
+        <iframe
+          src="https://movieznseriez.vercel.app/"
+          title="MoviesAndTVShows"
+          className="w-4/5 m-4 h-[40rem] rounded-md shadow-md shadow-gray-300"
+        ></iframe>
+        <a
           href="https://ankitxcoding.github.io/eCommerce/"
           className="m-4 text-white text-3xl font-semibold hover:text-yellow-500"
         >
@@ -32,17 +45,6 @@ const Projects = () => {
         <iframe
           src="https://ankitxcoding.github.io/eCommerce/"
           title="eCommerce"
-          className="w-4/5 m-4 h-[40rem] rounded-md shadow-md shadow-gray-300"
-        ></iframe>
-        <a
-          href="https://ankitxcoding.github.io/MoviesAndTVShows/"
-          className="m-4 text-white text-3xl font-semibold hover:text-amber-500"
-        >
-          <i className="fa-solid fa-link text-xl"></i> MoviesAndTVShows
-        </a>
-        <iframe
-          src="https://ankitxcoding.github.io/MoviesAndTVShows/"
-          title="MoviesAndTVShows"
           className="w-4/5 m-4 h-[40rem] rounded-md shadow-md shadow-gray-300"
         ></iframe>
         <FollowLinks />
